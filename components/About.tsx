f11e81eff4281308272a8049d7d07d0a1f8a0611
John McCants <johnmccants002@gmail.com>
2024-04-04 16:59:19 -0700

    Updated Styles

--- components/About.tsx
+++ components/About.tsx
@@ -1,30 +1,50 @@
-import React from 'react'
-import { View, Text, StyleSheet, Pressable, Image, TextInput } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Image,
+  TextInput,
+} from "react-native";
 // import QuestionMarkIcon from "../assets/images/question-mark-icon.png";
 
-
 const About = () => {
-
-
   return (
     <View style={styles.container}>
       <Text style={styles.textBold}>
         Tell us a little about yourself and your situation.
       </Text>
-      <Text style={styles.textBold}>
-        How can we help?
-      </Text>
-      <View styles={{flexDirection: "row", width: 500, height: 500, border: "solid", borderWidth:4, borderColor:"red", }}>
-        <TextInput style={{backgroundColor: "white", width: 240, placeholderTextColor: "gray", height: 35, borderRadius: 16, paddingHorizontal: 10, marginTop: 10, }} placeholder="Start typing here" />
-         <View >
-          <Image source={require("../assets/images/question-mark-icon.png")}/>
-         </View>
-         
+      <Text style={styles.textBold}>How can we help?</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 8,
+        }}
+      >
+        <TextInput
+          style={{
+            backgroundColor: "white",
+            width: 240,
+            color: "gray",
+            height: 35,
+            borderRadius: 16,
+            paddingHorizontal: 10,
+            marginTop: 10,
+          }}
+          placeholder="Start typing here"
+        />
+        <View>
+          <Image
+            source={require("../assets/images/question-mark-icon.png")}
+            style={{ height: 35, width: 35, marginTop: 4 }}
+          />
+        </View>
       </View>
     </View>
-  ) 
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -48,6 +68,4 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
-export default About
+export default About;
