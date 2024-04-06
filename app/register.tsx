1ee9db324947a2d0d673e794458930dce932f18a
John McCants <johnmccants002@gmail.com>
2024-04-05 18:36:55 -0700

    Add Login Screen. Create defaultStyles file for more uniform styling

---
+++ app/register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return <></>;
+};
+
+export default Page;
--- components/LogoContainer.tsx
+++ components/LogoContainer.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
 import { View, Image } from "react-native";
 
 const LogoContainer = () => {
   return (
-    <View style={{paddingHorizontal: 20, paddingTop:10, }}>
-    <Image
-      source={require("../assets/images/bridgelogo.png")}
+    <View
       style={{
-        resizeMode: "contain",
-        width: 100,
-        height: 60,
+        paddingHorizontal: 20,
+        paddingTop: 80,
+        backgroundColor: "#4BA4A4",
       }}
-    />
-  </View>
+    >
+      <Image
+        source={require("../assets/images/bridgelogo.png")}
+        style={{
+          resizeMode: "contain",
+          width: 100,
+          height: 60,
+        }}
+      />
+    </View>
+  );
+};
 
-  )
-}
-
-export default LogoContainer
+export default LogoContainer;
