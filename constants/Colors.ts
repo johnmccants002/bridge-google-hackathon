1996f05fafd0d5cb9b30103280b45eb0087f4c7a
John McCants <johnmccants002@gmail.com>
2024-04-09 19:58:40 -0700

    Rename SplashScreen to StartScreen. Update button in StartScreen to use CTAButton primary

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,27 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
+};
+
+export const Colors = {
+  accentLight: "#E9FFFF",
+  accentDark: "#125858",
+  accentPrimary: "#10AB8F",
+  black: "#000000E5",
+  white: "white",
 };
