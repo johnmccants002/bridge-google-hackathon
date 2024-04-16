8fae683ec8135d55d43396e0133442ed75d901ed
John McCants <johnmccants002@gmail.com>
2024-04-16 02:32:39 -0700

    Index back to normal

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
 import LogoContainer from "@/components/LogoContainer";
 import OnboardingScreen from "@/screens/onboarding/OnboardingScreen";
-import { Redirect } from "expo-router";
 
 interface Props {}
 
 const Page = (props: Props) => {
   return (
     <>
-      <Redirect href={"/results"} />
+      <LogoContainer />
+      <OnboardingScreen />
     </>
   );
 };
