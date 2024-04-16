0bf32caa9d3ab939d44cb1918ee106dedccffd4c
John McCants <johnmccants002@gmail.com>
2024-04-16 01:00:21 -0700

    Added edit demographics button and screen to navigate from ResourceScreen

---
+++ components/svgs/BoldCheck.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Svg, { SvgProps, Path } from "react-native-svg";
+const BoldCheck = (props: SvgProps) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <Path
+      fill="#084137"
+      fillRule="evenodd"
+      d="M21.546 5.111a1.5 1.5 0 0 1 0 2.121L10.303 18.475a1.6 1.6 0 0 1-2.263 0L2.454 12.89a1.5 1.5 0 1 1 2.121-2.121l4.596 4.596L19.424 5.111a1.5 1.5 0 0 1 2.122 0Z"
+      clipRule="evenodd"
+    />
+  </Svg>
+);
+export default BoldCheck;
