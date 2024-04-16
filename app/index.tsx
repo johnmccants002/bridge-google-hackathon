3eba0ef70f524f36ae46df03880f74954d36890c
John McCants <johnmccants002@gmail.com>
2024-04-16 01:54:22 -0700

    update index

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,13 @@
 import LogoContainer from "@/components/LogoContainer";
 import OnboardingScreen from "@/screens/onboarding/OnboardingScreen";
-import UserStoryScreen from "@/screens/user-story/UserStoryScreen";
-import { Redirect } from "expo-router";
 
 interface Props {}
 
 const Page = (props: Props) => {
   return (
     <>
-      {/* <LogoContainer />
-      <OnboardingScreen /> */}
-      <Redirect href={"/results"} />
+      <LogoContainer />
+      <OnboardingScreen />
     </>
   );
 };
