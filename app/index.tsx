818fc2f87b81ec2044cddd044b2f904cb35850a1
Eduardo Oliveira <edumottaoliveira@gmail.com>
2024-04-17 22:03:02 -0700

    Add api call test to register user

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
 import LogoContainer from "@/components/LogoContainer";
 import OnboardingScreen from "@/screens/onboarding/OnboardingScreen";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "@/constants/Colors"
 
 interface Props {}
 
 const Page = (props: Props) => {
   return (
-    <>
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.accentPrimary }}>
       <LogoContainer />
       <OnboardingScreen />
-    </>
+    </SafeAreaView>
   );
 };
 
