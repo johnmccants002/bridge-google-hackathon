092d39d5664455c0129253452f361f8b74811f5a
Miles Exner <miles.exner@csuglobal.edu>
2024-04-22 19:06:12 -0700

    ♻️ refactor singup and login, remove cognito

--- serverless/src/helpers.ts
+++ serverless/src/helpers.ts
@@ -1,6 +1,18 @@
+const bcrypt = require('bcrypt');
+
 export function generateSecretHash(username: string): string {
   const { createHmac } = require('crypto');
   return createHmac('SHA256', process.env.COGNITO_CLIENT_SECRET)
     .update(username + process.env.COGNITO_CLIENT_ID)
     .digest('base64');
+}
+
+export async function hashPassword(password: string): Promise<string> {
+  const saltRounds = 10; // Cost factor controls how much time is needed to calculate a single bcrypt hash.
+  const hash = await bcrypt.hash(password, saltRounds);
+  return hash;
+}
+
+export async function checkPassword(password: string, hash: string): Promise<boolean> {
+  return await bcrypt.compare(password, hash);
 }
--- serverless/src/signUp.ts
+++ serverless/src/signUp.ts
@@ -1,76 +1,61 @@
 /** Lambda Function
  * 
- * Post sign up to Cognito and user data to DynamoDB
+ * Post user data to DynamoDB
  * @param event {object} - Request object
  */
 
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
-import { CognitoIdentityProviderClient, SignUpCommand, SignUpCommandInput  } from '@aws-sdk/client-cognito-identity-provider';
-import { generateSecretHash } from './helpers';
-import AWSXRay from 'aws-xray-sdk-core';
+import { hashPassword } from './helpers';
 
 // Create a DynamoDB client
-const xrayWrappedDynamoDBClient = AWSXRay.captureAWSv3Client(new DynamoDBClient());
-const docClient = DynamoDBDocumentClient.from(xrayWrappedDynamoDBClient);
-const xrayWrappedCognitoClient = AWSXRay.captureAWSv3Client(new CognitoIdentityProviderClient({ region: process.env.AWS_REGION }));
+const ddbClient = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(ddbClient);
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  const { email, password, demographics } = JSON.parse(event.body || '{}');
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body || '{}');
 
-  if (!email || !password) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: 'Email and password are required' }),
-    };
-  }
-
-  try {
-    const signUpParams: SignUpCommandInput  = {
-      ClientId: process.env.COGNITO_CLIENT_ID,
-      Username: email,
-      Password: password,
-      SecretHash: generateSecretHash(email),
-    };
-    
-    const response = await xrayWrappedCognitoClient.send(new SignUpCommand(signUpParams));
-
-    console.log('Signup response:', response);
-
-    if (!response.UserConfirmed) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: 'User not confirmed' }),
-      };
-    }
-  } catch (error: any) {
-    console.error('Signup error:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: 'Signup failed, please try again.',
-        error: error.message,
-      }),
-    };
-  }
-
-  // If Cognito signup is successful, continue to store additional data in DynamoDB
   const TableName = process.env.USER_SCHEMA;
+
   if (!TableName) {
     console.error('Table name not set');
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Table name not set' }),
+      body: JSON.stringify({
+        message: 'Table name not set',
+      }),
     };
   }
 
-  // Store user data in DynamoDB;
-  const timestamp = new Date().getTime();
+  type Demographics = {
+    age: number;
+    income: number,
+    ethnicity: string,
+    veteran: boolean,
+    disability: boolean,
+    gender: string
+  }
+
+  const demographics: Demographics = data.demographics;
+  
+  const { id, password } = data;
+
+  if (!id || !password) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'ID and password are required' }),
+    };
+  }
+
+  const hashedPassword = await hashPassword(password);
+  
   const params = {
     TableName,
     Item: {
-      id: email, // Using email as the primary key
+      id: data.id,
+      password: hashedPassword,
       demographics,
       createdAt: timestamp,
       updatedAt: timestamp,
@@ -81,13 +66,15 @@ export const handler: APIGatewayProxyHandler = async (event, _context) => {
     await docClient.send(new PutCommand(params));
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Signup successful' }),
+      body: JSON.stringify(params.Item),
     };
   } catch (error) {
-    console.error('DynamoDB error:', error);
+    console.error(error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: "Couldn't post the data item." }),
+      body: JSON.stringify({
+        message: "Couldn't post the data item.",
+      }),
     };
   }
 };
