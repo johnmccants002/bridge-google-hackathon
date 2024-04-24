ad0b285345c907f4a73e67a31209756b9699838f
Miles Exner <miles.exner@csuglobal.edu>
2024-04-23 21:19:30 -0700

    ♻️ replace bcrypt with crypto

--- serverless/src/helpers.ts
+++ serverless/src/helpers.ts
@@ -1,18 +1,20 @@
-const bcrypt = require('bcrypt');
+import { pbkdf2Sync, randomBytes } from 'crypto';
+
+export function hashPassword(password: string): Promise<string> {
+  const salt = randomBytes(16).toString('hex');
+  const hash = pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+  return Promise.resolve(`${salt}:${hash}`);
+}
+
+export function checkPassword(password: string, hash: string): Promise<boolean> {
+  const [salt, key] = hash.split(':');
+  const derivedKey = pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+  return Promise.resolve(key === derivedKey);
+}
 
 export function generateSecretHash(username: string): string {
   const { createHmac } = require('crypto');
   return createHmac('SHA256', process.env.COGNITO_CLIENT_SECRET)
     .update(username + process.env.COGNITO_CLIENT_ID)
     .digest('base64');
-}
-
-export async function hashPassword(password: string): Promise<string> {
-  const saltRounds = 10; // Cost factor controls how much time is needed to calculate a single bcrypt hash.
-  const hash = await bcrypt.hash(password, saltRounds);
-  return hash;
-}
-
-export async function checkPassword(password: string, hash: string): Promise<boolean> {
-  return await bcrypt.compare(password, hash);
 }
